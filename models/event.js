510096e07827e58a7767b0818429195a3983ba99
Geoff Scott <geoff@hackerati.us>
2016-05-09 10:43:12 -0400

    return proper HTTP status codes based on event type

--- models/event.js
+++ models/event.js
@@ -1,4 +1,5 @@
 var neo4j = require('neo4j');
+var moment = require('moment');
 
 var db = new neo4j.GraphDatabase({
     url: process.env['NEO4J_URL'] || process.env['GRAPHENEDB_URL'] ||
@@ -10,10 +11,42 @@ var Event = module.exports = function Event(_node) {
     this._node = _node;
 }
 
-Event.create = function (params, callback) {
+/*
+ * Define Github webhook event types. See docs for details:
+ *
+ * https://developer.github.com/v3/activity/events/types/
+ */
+Event.GITHUB_COMMIT_COMMENT = "commit_comment";
+Event.GITHUB_CREATE = "create";
+Event.GITHUB_DELETE = "delete";
+Event.GITHUB_DEPLOYMENT = "deployment";
+Event.GITHUB_DEPLOYMENT_STATUS = "deployment_status";
+Event.GITHUB_FORK = "fork";
+Event.GITHUB_EVENT_GOLLUM = "gollum";
+Event.GITHUB_ISSUE_COMMENT = "issue_comment";
+Event.GITHUB_ISSUES = "issues";
+Event.GITHUB_MEMBER = "member";
+Event.GITHUB_MEMBERSHIP = "membership";
+Event.GITHUB_PAGE_BUILD = "page_build";
+Event.GITHUB_PUBLIC = "public";
+Event.GITHUB_PULL_REQUEST_COMMENT = "pull_request_review_comment";
+Event.GITHUB_PULL_REQUEST = "pull_request";
+Event.GITHUB_PUSH = "push";
+Event.GITHUB_REPOSITORY = "repository";
+Event.GITHUB_RELEASE = "release";
+Event.GITHUB_STATUS = "status";
+Event.GITHUB_TEAM_ADD = "team_add";
+Event.GITHUB_WATCH = "watch";
+
+Event.createPush = function (event_id, event_payload, callback) {
     db.cypher({
         query: 'CREATE (event:Event { id : {id}, timestamp : {timestamp}, event_type: {event_type}, payload: {payload} }) RETURN event',
-        params: params,
+        params: {
+            id: event_id,
+            timestamp: moment().format(),
+            event_type: Event.GITHUB_PUSH,
+            payload: JSON.stringify(event_payload, null, 2)
+        },
     }, function (err, results) {
         if (err) return callback(err);
         var event = new Event(results[0]['event']);
--- routes/events.js
+++ routes/events.js
@@ -1,22 +1,117 @@
 var URL = require('url');
-var moment = require('moment');
 
 var Event = require('../models/event');
 
 exports.create = function (req, res, next) {
     if(!req.isXHub || !req.isXHubValid()){
-        return res.status(403).json({ error: 'unauthorized' });
+        return res.status(403).json({ error: 'Unauthorized' });
     }
 
-    Event.create({
-        id: req.get('X-GitHub-Delivery'),
-        timestamp: moment().format(),
-        event_type: req.get('X-GitHub-Event'),
-        payload: JSON.stringify(req.body, null, 2)
-    }, function (err, event) {
-        if (err) throw err;
+    var event_id = req.get('X-GitHub-Delivery');
+    var event_type = req.get('X-GitHub-Event');
 
-        console.log(event);
-        res.status(201).json(event);
-    });
+    switch (event_type) {
+        case Event.GITHUB_COMMIT_COMMENT:
+            // Any time a Commit is commented on.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_CREATE:
+            // Any time a Branch or Tag is created.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_DELETE:
+            // Any time a Branch or Tag is deleted.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_DEPLOYMENT:
+            // Any time a Repository has a new deployment created from the API
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_DEPLOYMENT_STATUS:
+            // Any time a deployment for a Repository has a status update
+            // from the API.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_FORK:
+            // Any time a Repository is forked.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_GOLLUM:
+            // Any time a Wiki page is updated.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_ISSUE_COMMENT:
+            // Any time a comment on an issue is created, edited, or deleted.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_ISSUES:
+            // Any time an Issue is assigned, unassigned, labeled, unlabeled,
+            // opened, edited, closed, or reopened.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_MEMBER:
+            // Any time a User is added as a collaborator to a non-
+            // Organization Repository.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_MEMBERSHIP:
+            // Any time a User is added or removed from a team. Organization
+            // hooks only.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_PAGE_BUILD:
+            // Any time a Pages site is built or results in a failed build.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_PUBLIC:
+            // Any time a Repository changes from private to public.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_PULL_REQUEST_COMMENT:
+            // Any time a comment on a Pull Request's unified diff is created,
+            // edited, or deleted (in the Files Changed tab).
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_PULL_REQUEST:
+            // Any time a Pull Request is assigned, unassigned, labeled,
+            // unlabeled, opened, edited, closed, reopened, or synchronized
+            // (updated due to a new push in the branch that the pull request
+            // is tracking).
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_PUSH:
+            // Any Git push to a Repository, including editing tags or
+            // branches. Commits via API actions that update references are
+            // also counted. This is the default event.
+            Event.createPush(event_id, req.body, function (err, event) {
+                if (err) throw err;
+                console.log(event);
+                return res.status(201).json(event);
+            });
+            break;
+        case Event.GITHUB_REPOSITORY:
+            // Any time a Repository is created, deleted, made public, or
+            // made private.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_RELEASE:
+            // Any time a Release is published in a Repository.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_STATUS:
+            // Any time a Repository has a status update from the API
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_TEAM_ADD:
+            // Any time a team is added or modified on a Repository.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        case Event.GITHUB_WATCH:
+            // Any time a User stars a Repository.
+            return res.status(501).json({ error:
+                                          `Not Implemented: ${event_type}` });
+        default:
+            return res.status(400).json({ error:
+                                          `Unknown Event: ${event_type}` });
+    }
 };
