a64ef803b6178f4305a60342dee39152fa03e66a
Geoff Scott <geoff@hackerati.us>
2016-05-30 13:08:42 -0400

    describe desired HTTP status codes to return

--- routes/events.js
+++ routes/events.js
@@ -5,6 +5,16 @@ var GithubEventNotImplementedError = require('../lib/github_events/errors')
 var GithubEventNotValidError = require('../lib/github_events/errors')
 var Event = require('../models/event')
 
+//
+// HTTP Status             Result
+// -----------             ------ 
+// 201 Created             Event successfully processed
+// 403 Forbidden           Missing or invalid X-Hub-Signature
+// 501 Not Implemented     Invalid X-GitHub-Event
+// 400 Bad Request         Invalid JSON Payload
+// 502 Bad Gateway         Error from neo4j API call
+// 504 Gateway Timeout     Timeout from neo4j API call
+//
 exports.create = function (req, res, next) {
     if(!req.isXHub || !req.isXHubValid()){
         return res.status(403).json({ error: 'Unauthorized' })
