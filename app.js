9c02b9bfb3d41037e343210eeb3043304dfe8c76
Geoff Scott <geoff@hackerati.us>
2016-05-30 17:01:34 -0400

    make route handler a class

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var xhub = require('express-x-hub');
-var neo4j = require('neo4j');
-var routes = require('./routes')
+var express = require('express')
+var bodyParser = require('body-parser')
+var xhub = require('express-x-hub')
+var neo4j = require('neo4j')
 var EventModel = require('./models/event')
+var EventRoutes = require('./routes/events')
 
-// treat Event as a singleton; save it's database connection
+// Event is a singleton; save it's database connection
 var db = new neo4j.GraphDatabase({
     url: process.env['NEO4J_URL'] || process.env['GRAPHENEDB_URL'] ||
         'http://neo4j:neo4j@localhost:7474',
     auth: process.env['NEO4J_AUTH'],
-});
+})
 
-EventModel.setDatabaseConnection (db);
+EventModel.setDatabaseConnection (db)
 
-var app = express();
-var port = 3000;
+var app = express()
+var port = 3000
 
 // Middleware to validate Github X-Hub-Signature
-app.use(xhub({ algorithm: 'sha1', secret: process.env.X_HUB_SECRET }));
+app.use(xhub({ algorithm: 'sha1', secret: process.env.X_HUB_SECRET }))
 
 // Middleware to parse JSON requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Github Webhook handler
-app.post('/event', routes.events.create);
+app.post('/event', EventRoutes.create)
 
 app.listen(port, (error) => {
     if (error) {
@@ -33,4 +33,4 @@ app.listen(port, (error) => {
     } else {
         console.info ("==> 🌎  Listening on port %s.", port)
     }
-});
+})
--- routes/events.js
+++ routes/events.js
@@ -1,39 +1,49 @@
-var URL = require('url')
+'use strict'
 
 var GithubEventFactory = require('../lib/github_events/factory')
 var GithubEventNotImplementedError = require('../lib/github_events/errors')
 var GithubEventNotValidError = require('../lib/github_events/errors')
-var Event = require('../models/event')
 
-//
-// HTTP Status             Result
-// -----------             ------ 
-// 201 Created             Event successfully processed
-// 403 Forbidden           Missing or invalid X-Hub-Signature
-// 501 Not Implemented     Invalid X-GitHub-Event
-// 400 Bad Request         Invalid JSON Payload
-// 502 Bad Gateway         Error from neo4j API call
-// 504 Gateway Timeout     Timeout from neo4j API call
-//
-exports.create = function (req, res, next) {
-    if(!req.isXHub || !req.isXHubValid()){
-        return res.status(403).json({ error: 'Unauthorized' })
-    }
+module.exports = class Event {
+    // 
+    // static create ():
+    //
+    // POST event callback to handler
+    //
+    // @param req - request
+    // @param res - response
+    // @param next - next handler
+    // @returns json - response status & json
+    //
+    // HTTP Status             Result
+    // -----------             ------ 
+    // 201 Created             Event successfully processed
+    // 403 Forbidden           Missing or invalid X-Hub-Signature
+    // 501 Not Implemented     Invalid X-GitHub-Event
+    // 400 Bad Request         Invalid JSON Payload
+    // 502 Bad Gateway         Error from neo4j API call
+    // 504 Gateway Timeout     Timeout from neo4j API call
+    //
+    static create (req, res, next) {
+        if(!req.isXHub || !req.isXHubValid()){
+            return res.status(403).json({ error: 'Unauthorized' })
+        }
 
-    var event_id = req.get('X-GitHub-Delivery')
-    var event_type = req.get('X-GitHub-Event')
+        var event_id = req.get('X-GitHub-Delivery')
+        var event_type = req.get('X-GitHub-Event')
 
-    try {
-        var event = GithubEventFactory.createEvent (event_id, event_type, req.body)
-        event.save ((err) => {
-            if (err) {
-                console.log (err)
-                return res.status(502).json({ error: err })
-            }
-            return res.status(201).json({ success: 'Created' })
-        })
-    } catch (err) {
-	console.log (err)
-        return res.status(501).json({ error: err })
+        try {
+            var event = GithubEventFactory.createEvent (event_id, event_type, req.body)
+            event.save ((err) => {
+                if (err) {
+                    console.error (err)
+                    return res.status(502).json({ error: err })
+                }
+                return res.status(201).json({ success: 'Created' })
+            })
+        } catch (err) {
+	    console.error (err)
+            return res.status(501).json({ error: err })
+        }
     }
 }
